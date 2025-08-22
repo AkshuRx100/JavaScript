220e0e32bd778cec3803e5b58bfee806f836b3e9
AkshuRX100 <akshutelang1702@gmail.com>
2025-08-22 21:54:43 +0530

    changes by akshay

---
+++ Arrays.js
@@ -0,0 +1,15 @@
+let DJ = ['Martin garix','David','skrillix','afro']
+
+// let x;
+// for(x in DJ)
+// {
+//     console.log(DJ[x]);
+// }
+
+
+let x = DJ.slice(0,2);
+
+console.log(x);
+
+x.push("NKY");
+console.log(x);
